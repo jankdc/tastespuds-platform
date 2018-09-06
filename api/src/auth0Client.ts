904574619cc6344768fe05476239bfbfe0ff9180
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-09-06 17:03:49 +0100

    Adds simple auth0 client

---
+++ api/src/auth0Client.ts
@@ -0,0 +1,54 @@
+import * as env from 'env-var';
+import * as request from 'request-promise-native';
+
+const AUTH0_CLIENT_ID = env.get('AUTH0_CLIENT_ID').required().asString();
+const AUTH0_DOMAIN_URL = env.get('AUTH0_DOMAIN_URL').required().asUrlString();
+const AUTH0_CLIENT_SECRET = env.get('AUTH0_CLIENT_SECRET').required().asString();
+
+export interface SignInTokens {
+  access_token: string,
+  refresh_token: string,
+  id_token: string,
+  token_type: string,
+  expires_in: number
+}
+
+export interface IdToken {
+  name?: string,
+  email?: string,
+  picture?: string,
+  sub: string,
+  iss: string,
+  aud: string,
+  exp: string,
+  iat: string
+}
+
+export async function getTokens(code: string, redirectUri: string) {
+  interface RequestBody {
+    client_secret: string
+    redirect_uri: string
+    grant_type: string
+    client_id: string
+    code: string
+  }
+
+  const body: RequestBody = {
+    client_secret: AUTH0_CLIENT_SECRET,
+    redirect_uri: redirectUri,
+    grant_type: 'authorization_code',
+    client_id: AUTH0_CLIENT_ID,
+    code
+  };
+
+  const options: request.OptionsWithUri = {
+    method: 'POST',
+    json: true,
+    body,
+    uri: `${AUTH0_DOMAIN_URL}/oauth/token`
+  };
+
+  const response: SignInTokens = await request(options);
+
+  return response;
+}
