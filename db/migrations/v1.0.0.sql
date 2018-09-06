1357c8898e045a6c41fba742df8e122057177f00
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-09-06 13:44:30 +0100

    Add user table

--- db/migrations/v1.0.0.sql
+++ db/migrations/v1.0.0.sql
@@ -2,4 +2,13 @@
 -- Tables
 ------------------------------------
 
+-- user
+
+CREATE TABLE tastespuds.user (
+  id TEXT PRIMARY KEY NOT NULL,
+
+  CONSTRAINT no_empty_id
+    CHECK (id != '')
+);
+
 INSERT INTO tastespuds.db_version (version) VALUES ('{1,0,0}');
