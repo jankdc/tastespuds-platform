a003d962f278a8aac889acae510dfdb3151f3d8c
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-09-19 12:41:13 +0100

    Adds minimal db tables

--- db/migrations/v1.0.0.sql
+++ db/migrations/v1.0.0.sql
@@ -11,4 +11,119 @@ CREATE TABLE tastespuds.user (
     CHECK (id != '')
 );
 
+-- place
+
+CREATE TABLE tastespuds.place (
+  id SERIAL PRIMARY KEY NOT NULL,
+  gplace_id TEXT NOT NULL,
+
+  CONSTRAINT no_empty_gplace_id
+    CHECK (gplace_id != '')
+);
+
+-- item
+
+CREATE TABLE tastespuds.item (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  place_id INTEGER NOT NULL,
+  creation_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+
+  CONSTRAINT no_empty_name
+    CHECK (name != ''),
+
+  FOREIGN KEY (place_id)
+    REFERENCES tastespuds.place (id)
+);
+
+-- review
+
+CREATE TABLE tastespuds.review (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assets TEXT[] NOT NULL,
+  rating NUMERIC(2,1) NOT NULL,
+  item_id INTEGER NOT NULL,
+  user_id TEXT NOT NULL,
+  content TEXT NOT NULL,
+  creation_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+
+  CONSTRAINT no_empty_assets
+    CHECK (array_length(assets, 1) != 0),
+
+  CONSTRAINT no_empty_content
+    CHECK (content != ''),
+
+  CONSTRAINT rating_must_be_in_range
+    CHECK (rating >= 0 AND rating <= 10),
+
+  FOREIGN KEY (user_id)
+    REFERENCES tastespuds.user (id),
+
+  FOREIGN KEY (item_id)
+    REFERENCES tastespuds.item (id)
+);
+
+-- comment
+
+CREATE TABLE tastespuds.comment (
+  id SERIAL PRIMARY KEY NOT NULL,
+  content TEXT NOT NULL,
+  user_id TEXT NOT NULL,
+  parent_id INTEGER NULL,
+  review_id INTEGER NOT NULL,
+  creation_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+
+  CONSTRAINT no_empty_content
+    CHECK (content != ''),
+
+  CONSTRAINT no_circular_comments
+    CHECK (id != parent_id),
+
+  FOREIGN KEY (user_id)
+    REFERENCES tastespuds.user (id)
+    ON DELETE CASCADE,
+
+  FOREIGN KEY (parent_id)
+    REFERENCES tastespuds.comment (id)
+    ON DELETE CASCADE,
+
+  FOREIGN KEY (review_id)
+    REFERENCES tastespuds.review (id)
+    ON DELETE CASCADE
+);
+
+-- like_review
+
+CREATE TABLE tastespuds.like_review (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id TEXT NOT NULL,
+  review_id INTEGER NOT NULL,
+  creation_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+
+  FOREIGN KEY (user_id)
+    REFERENCES tastespuds.user (id)
+    ON DELETE CASCADE,
+
+  FOREIGN KEY (review_id)
+    REFERENCES tastespuds.review (id)
+    ON DELETE CASCADE
+);
+
+-- like_comment
+
+CREATE TABLE tastespuds.like_comment (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id TEXT NOT NULL,
+  comment_id INTEGER NOT NULL,
+  creation_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+
+  FOREIGN KEY (user_id)
+    REFERENCES tastespuds.user (id)
+    ON DELETE CASCADE,
+
+  FOREIGN KEY (comment_id)
+    REFERENCES tastespuds.comment (id)
+    ON DELETE CASCADE
+);
+
 INSERT INTO tastespuds.db_version (version) VALUES ('{1,0,0}');
