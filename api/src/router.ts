c4965273ed632762d0253f7046202d522b67a728
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-10-09 14:09:52 +0100

    Adds it to router

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,11 +1,13 @@
 import * as Router from 'koa-router'
 import * as reviews from './reviews'
+import * as places from './places'
 import * as users from './users'
 import * as oauth from './oauth'
 
 const router = new Router()
 router.use(oauth.routes())
 router.use(users.routes())
+router.use(places.routes())
 router.use(reviews.routes())
 
 export const routes = () => router.routes()
