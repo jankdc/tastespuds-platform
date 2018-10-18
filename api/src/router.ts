70016e6f9fea4cbe3c0c40b71c1e75d65a742282
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-10-18 17:05:07 +0100

    Connects to main routes

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,5 +1,6 @@
 import * as Router from 'koa-router'
 import * as reviews from './reviews'
+import * as assets from './assets'
 import * as search from './search'
 import * as places from './places'
 import * as items from './items'
@@ -11,6 +12,7 @@ router.use(items.routes())
 router.use(oauth.routes())
 router.use(users.routes())
 router.use(places.routes())
+router.use(assets.routes())
 router.use(search.routes())
 router.use(reviews.routes())
 
