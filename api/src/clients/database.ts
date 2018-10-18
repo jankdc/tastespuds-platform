345d2b08a43f6e0389aac7d97592230c686b43b5
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-10-18 17:02:33 +0100

    Adds transaction to database interface

--- api/src/clients/database.ts
+++ api/src/clients/database.ts
@@ -1,14 +1,22 @@
-import { Pool, QueryResult } from 'pg'
+import { Pool, QueryResult, PoolClient } from 'pg'
 import * as util from 'util'
 import * as fs from 'fs'
 
 import * as config from '../config'
 
 export interface DatabaseClient {
+  createTransaction(task: Task): Promise<void>
+
+  queryClientViaFile(client: PoolClient, path: string, values?: any[]): Promise<QueryResult>
+  queryClientViaText(client: PoolClient, text: string, values?: any[]): Promise<QueryResult>
+
   queryViaFile(path: string, values?: any[]): Promise<QueryResult>
-  queryViaText(path: string, values?: any[]): Promise<QueryResult>
+  queryViaText(text: string, values?: any[]): Promise<QueryResult>
+
   close(): Promise<void>
 }
+
+export type Task = (client: PoolClient) => Promise<void>
 
 const pool = new Pool({
   connectionString: config.databaseUrl
@@ -17,11 +25,32 @@ const pool = new Pool({
 const readFileAsync = util.promisify(fs.readFile)
 
 const databaseClient: DatabaseClient = {
-  queryViaFile: async (path: string, values?: any[]) => {
+  createTransaction: async (task) => {
+    const client = await pool.connect()
+
+    try {
+      await client.query('BEGIN')
+      await task(client)
+      await client.query('COMMIT')
+    } catch (e) {
+      await client.query('ROLLBACK')
+      throw e
+    } finally {
+      client.release()
+    }
+  },
+  queryClientViaFile: async (client, path, values?) => {
+    const file = await readFileAsync(path)
+    return client.query(file.toString(), values)
+  },
+  queryClientViaText: async (client, text, values?) => {
+    return client.query(text, values)
+  },
+  queryViaFile: async (path, values?) => {
     const file = await readFileAsync(path)
     return pool.query(file.toString(), values)
   },
-  queryViaText: async (text: string, values?: any[]) => {
+  queryViaText: async (text, values?) => {
     return pool.query(text, values)
   },
   close: async () => {
