96e26c1c4bfbac44f2e78c83237e72fa8e1ea89d
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-10-18 17:03:22 +0100

    Check multipart form data input

--- api/src/input.ts
+++ api/src/input.ts
@@ -7,7 +7,10 @@ export function createValidator(schema: any): Koa.Middleware {
   const validate = ajv.compile(schema)
 
   return async (ctx, next) => {
-    const input = Object.assign({}, ctx.request.body, ctx.query)
+    // koa-multer issue https://github.com/koa-modules/multer/issues/14
+    const req = ctx.req as any
+
+    const input = Object.assign({}, ctx.request.body, ctx.query, req.body)
     if (validate(input)) {
       return await next()
     }
