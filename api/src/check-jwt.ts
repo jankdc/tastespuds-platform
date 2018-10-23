646c6851cb15a4f15a6ec9e7d0d1dcbfa76aada0
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-10-23 15:26:24 +0100

    Fixes bad import and force type coercion

--- api/src/check-jwt.ts
+++ api/src/check-jwt.ts
@@ -1,19 +1,23 @@
 import * as Koa from 'koa'
 import * as jwt from 'koa-jwt'
-import * as rsa from 'koa-rsa'
+import * as rsa from 'jwks-rsa'
 
 import * as config from './config'
 
 export function createCheckJwtMiddleware(): Koa.Middleware {
+  const secretLoader = rsa.koaJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 2,
+    jwksUri: `${config.jwksHost}/.well-known/jwks.json`
+  })
+
   return jwt({
-    secret: rsa.koaJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 2,
-      jwksUri: `${config.jwksHost}/.well-known/jwks.json`
-    }),
-    audience: config.jwksAudience,
+    // NOTE: Forced type coercion because of bad typing
+    // https://github.com/auth0/node-jwks-rsa/issues/46
+    secret: secretLoader as jwt.SecretLoader,
     issuer: `${config.jwksHost}/`,
+    audience: config.jwksAud,
     algorithms: [ 'RS256' ]
   })
 }
