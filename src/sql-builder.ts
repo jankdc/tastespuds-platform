3972c600919b468e6fda35d17cd987e8321bb74c
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2018-12-15 21:55:30 +0000

    Register array types

--- src/sql-builder.ts
+++ src/sql-builder.ts
@@ -1,3 +1,8 @@
 import * as squel from 'squel'
 
-export default squel.useFlavour('postgres')
+const client = squel.useFlavour('postgres')
+client.registerValueHandler(Array, (array) => {
+  return 'ARRAY(SELECT json_array_elements_text(\'' + JSON.stringify(array) + '\'))'
+})
+
+export default client
